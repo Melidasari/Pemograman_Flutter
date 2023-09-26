12d0ea3ac02726622e084c7ecb30be6bcb7886b3
Melidasari <melidasari163@gmail.com>
2023-09-26 12:42:39 +0700

    Update FLUTTER 05 Text Widget

--- first_project/lib/main.dart
+++ first_project/lib/main.dart
@@ -10,13 +10,28 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.lime,
-        body: Center(
-          child: Text("Hello World"),
-        ),
         appBar: AppBar(
           title: Text("Melida Sari"),
         ),
+        body: Center(
+            child: Text(
+          "Assalamualaikum Perkenalkan Nama Saya Melida Sari kelas TK 2 A Mata Kuliah Pemograman Mobile Lanjut (FLUTTER)",
+          maxLines: 7,
+          overflow: TextOverflow.ellipsis,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            backgroundColor: Colors.cyan,
+            color: Colors.white,
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 10,
+            fontFamily: 'ShadowsIntoLight',
+            decorationStyle: TextDecorationStyle.dotted,
+            decoration: TextDecoration.lineThrough,
+            decorationColor: Colors.green,
+            decorationThickness: 2,
+          ),
+        )),
       ),
     );
   }
