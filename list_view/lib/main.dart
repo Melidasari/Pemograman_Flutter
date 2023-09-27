42892dbd29f8beb97fddeafd697fae6e1afe239b
Melidasari <melidasari163@gmail.com>
2023-09-27 20:01:24 +0700

    Update main.dart FLUTTER 07 List View

--- list_view/lib/main.dart
+++ list_view/lib/main.dart
@@ -5,28 +5,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  List<Widget> myList = [
-    Container(
-      height: 300,
-      width: 300,
-      color: Colors.blueGrey,
-    ),
-    Container(
-      height: 300,
-      width: 300,
-      color: Colors.lightGreen,
-    ),
-    Container(
-      height: 300,
-      width: 300,
-      color: Colors.red,
-    ),
-    Container(
-      height: 300,
-      width: 300,
-      color: Colors.yellow,
-    ),
-  ];
+  List<Color> myColor = [Colors.red, Colors.yellow, Colors.green, Colors.grey];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -34,9 +13,21 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("List View"),
         ),
-        body: ListView(
-          // scrollDirection: Axis.horizontal,
-          children: myList,
+        body: ListView.separated(
+          separatorBuilder: (context, index) {
+            return Container(
+              height: 50,
+              color: Colors.white,
+            );
+          },
+          itemCount: myColor.length,
+          itemBuilder: (context, index) {
+            return Container(
+              height: 300,
+              width: 300,
+              color: myColor[index],
+            );
+          },
         ),
       ),
     );
