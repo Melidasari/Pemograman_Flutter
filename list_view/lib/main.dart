03b295193adf9d1b70c0bda87536bd11d6308a1c
Melidasari <melidasari163@gmail.com>
2023-09-27 19:34:16 +0700

    Update main.dart

--- list_view/lib/main.dart
+++ list_view/lib/main.dart
@@ -12,26 +12,26 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("List View"),
         ),
-        body: Column(
+        body: ListView(
           children: [
             Container(
-              height: 100,
-              width: 100,
+              height: 250,
+              width: 250,
               color: Colors.blueGrey,
             ),
             Container(
-              height: 100,
-              width: 100,
+              height: 250,
+              width: 250,
               color: Colors.lightGreen,
             ),
             Container(
-              height: 100,
-              width: 100,
+              height: 250,
+              width: 250,
               color: Colors.red,
             ),
             Container(
-              height: 100,
-              width: 100,
+              height: 250,
+              width: 250,
               color: Colors.yellow,
             ),
           ],
