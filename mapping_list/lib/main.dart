41e70a56c5734f8e4795294ba013aa3ee71ea4bb
Melidasari <melidasari163@gmail.com>
2023-10-11 23:21:13 +0700

    Update FLUTTER 12 Mapping List

--- mapping_list/lib/main.dart
+++ mapping_list/lib/main.dart
@@ -21,6 +21,16 @@ class MyApp extends StatelessWidget {
       "Age": 21,
       "favcolor": ["White", "Green", "Black", "Amber"]
     },
+    {
+      "Name": "Ukkaisya Renold",
+      "Age": 27,
+      "favcolor": ["Black", "White", "Green", "Blue", "White", "Green"],
+    },
+    {
+      "Name": "Zainuddin",
+      "Age": 21,
+      "favcolor": ["White", "Green", "Black", "Amber"]
+    },
   ];
   @override
   Widget build(BuildContext context) {
@@ -38,38 +48,47 @@ class MyApp extends StatelessWidget {
             return Card(
               margin: EdgeInsets.all(20),
               color: Colors.blueGrey.withOpacity(0.1),
-              child: Column(
-                children: [
-                  //row
-                  Row(
-                    children: [
-                      CircleAvatar(),
-                      Column(
-                        children: [
-                          Text("Name: ${data['Name']}"),
-                          Text("Age : ${data['Age']}"),
-                        ],
-                      ),
-                    ],
-                  ),
-                  //favcolor
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      children: myfavcolor.map((Color) {
-                        return Container(
-                          color: Colors.yellow,
-                          margin: EdgeInsets.symmetric(
-                            vertical: 15,
-                            horizontal: 8,
-                          ),
-                          padding: EdgeInsets.all(10),
-                          child: Text(Color),
-                        );
-                      }).toList(),
+              child: Container(
+                margin: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    //row
+                    Row(
+                      children: [
+                        CircleAvatar(),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Name: ${data['Name']}"),
+                            Text("Age : ${data['Age']}"),
+                          ],
+                        ),
+                      ],
                     ),
-                  ),
-                ],
+                    //favcolor
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: myfavcolor.map((Color) {
+                          return Container(
+                            color: Colors.yellow,
+                            margin: EdgeInsets.symmetric(
+                              vertical: 15,
+                              horizontal: 8,
+                            ),
+                            padding: EdgeInsets.all(10),
+                            child: Text(Color),
+                          );
+                        }).toList(),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             );
           }).toList(),
