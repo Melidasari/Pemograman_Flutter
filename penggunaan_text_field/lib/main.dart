efbc7dd3db44507bdb6816fa8e804d735ebfafd1
Melidasari <melidasari163@gmail.com>
2023-10-19 01:41:47 +0700

    Update FLUTTER 18 Penggunaan TextField

--- penggunaan_text_field/lib/main.dart
+++ penggunaan_text_field/lib/main.dart
@@ -4,30 +4,46 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  final TextEditingController myController = TextEditingController();
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TextEditingController myController = TextEditingController();
+  String hasil = "";
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Text Field"),
+          title: Text("Text Field"), // AppBar
         ),
         body: Center(
           child: Padding(
             padding: const EdgeInsets.all(25.0),
-            child: TextField(
-              controller: myController,
-              onChanged: (value) {
-                print("ONCHANGE: $value");
-              },
-              onSubmitted: (value) {
-                print("SUBMITTED: $value");
-              },
-              onEditingComplete: () {
-                print("EDIT SUCCESS");
-              },
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                TextField(
+                  controller: myController,
+                  onChanged: (value) {
+                    print("ONCHANGE");
+                  },
+                  onSubmitted: (value) {
+                    print(value);
+                    setState(() {
+                      hasil = value;
+                    });
+                  },
+                  onEditingComplete: () {
+                    print("EDIT SUCCESS");
+                    print(myController.text);
+                  },
+                ),
+                Text(hasil),
+              ],
             ),
           ),
         ),
