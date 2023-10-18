3b8c6430c86d4967deeacc464d403bc817663f7b
Melidasari <melidasari163@gmail.com>
2023-10-18 23:10:59 +0700

    Upload FLUTTER 15 TabBar Widget

--- tabbar_widget/lib/main.dart
+++ tabbar_widget/lib/main.dart
@@ -5,6 +5,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  //DefaultTabController myController = DefaultTabController(length: 3, child: null)
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,7 +13,25 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("My Apps"),
           bottom: PreferredSize(
-              preferredSize: Size.fromHeight(200), child: Text("Bottom")),
+            preferredSize: Size.fromHeight(50),
+            child: DefaultTabController(
+              length: 3,
+              child: TabBar(
+                tabs: [
+                  Tab(
+                    text: "Tab 1",
+                  ),
+                  Tab(
+                    text: "Tab 2",
+                  ),
+                  Tab(
+                    text: "Tab 3",
+                  ),
+                ],
+                //preferredSize: Size.fromHeight(200), child: Text("Bottom")
+              ),
+            ),
+          ),
         ),
       ),
     );
