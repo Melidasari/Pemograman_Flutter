f5c03803340060c132e6b072d889d867fafd53f4
Melidasari <melidasari163@gmail.com>
2023-10-25 21:36:08 +0700

    Update FLUTTER 23 Routes

--- routes/lib/main.dart
+++ routes/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import './home_page.dart';
+import './pages/gallery_page.dart';
+import './pages/photo_page.dart';
+import './pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,6 +12,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: HomePage(),
+      initialRoute: HomePage.nameRoute,
+      routes: {
+        HomePage.nameRoute: (context) => HomePage(),
+        GalleryPage.nameRoute: (context) => GalleryPage(),
+        PhotoPage.nameRoute: (context) => PhotoPage(),
+      },
     );
   }
 }
