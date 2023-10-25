60623ca7958d64ba2cdbff3487dfe9048181131f
Melidasari <melidasari163@gmail.com>
2023-10-25 23:27:04 +0700

    Update FLUTTER 26 Model

--- model_26/lib/pages/home_page.dart
+++ model_26/lib/pages/home_page.dart
@@ -1,10 +1,11 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:faker/faker.dart';
 import '../models/product.dart';
 
 class HomePage extends StatelessWidget {
+  final Faker faker = Faker();
+
   @override
   Widget build(BuildContext context) {
     List<Product> dummyData = List.generate(100, (index) {
@@ -16,25 +17,55 @@ class HomePage extends StatelessWidget {
       );
     });
 
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Marketplace"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Marketplace"),
+      ),
+      body: GridView.builder(
+        padding: EdgeInsets.all(10),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
         ),
-        body: GridView.builder(
-          padding: EdgeInsets.all(10),
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            mainAxisSpacing: 10,
-            crossAxisSpacing: 10,
-          ),
-          itemBuilder: (context, index) {
-            return GridTile(
-              child: Image.network(dummyData[index].imageURL),
-            );
-          },
-          itemCount: dummyData.length,
-        ),
+        itemBuilder: (context, index) {
+          return GridTile(
+            child: Image.network(dummyData[index].imageURL),
+            footer: Container(
+              height: 80,
+              color: Colors.green.withOpacity(0.8),
+              alignment: Alignment.center,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    dummyData[index].judul,
+                    maxLines: 1,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    "Rp ${dummyData[index].harga}",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    dummyData[index].deskripsi,
+                    maxLines: 2,
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+        itemCount: dummyData.length,
       ),
     );
   }
