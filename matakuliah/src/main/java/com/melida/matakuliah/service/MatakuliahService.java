2aff0cb94cbd1ed424e7d52540c48ac2584b6d3f
Melidasari <melidasari163@gmail.com>
2023-12-14 15:49:30 +0700

    Upload LATIHAN NETBEANS Matakuliah

---
+++ matakuliah/src/main/java/com/melida/matakuliah/service/MatakuliahService.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.melida.matakuliah.service;
+
+import com.melida.matakuliah.entity.Matakuliah;
+import com.melida.matakuliah.repository.MatakuliahRepository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author abdullah adam
+ */
+@Service
+public class MatakuliahService {
+    private final MatakuliahRepository matakuliahRepository;
+    @Autowired
+    public MatakuliahService(MatakuliahRepository matakuliahRepository){
+        this.matakuliahRepository = matakuliahRepository;
+    }
+    
+    public Matakuliah getMahasiswa(Long idmahasiswa){
+        return matakuliahRepository.findById(idmahasiswa).get();
+    }
+    
+    public List<Matakuliah> getAll(){
+        return matakuliahRepository.findAll();
+    }
+    
+    public void insert(Matakuliah matakuliah){
+        Optional<Matakuliah> matakuliahOptional = 
+                matakuliahRepository.findMatakuliahByKode(matakuliah.getKode());
+        if(matakuliahOptional.isPresent()){
+            throw new IllegalStateException("Email Sudah Ada");
+        }
+        matakuliahRepository.save(matakuliah);
+    }
+}
